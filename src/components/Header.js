a7f91cc4bc0ab58837cabbb4cda51af8d0dd558b
Dibyajyoti Pradhan <dibyojyotipradhan@gmail.com>
2024-10-28 03:51:22 +0530

    Configure for GitHub Pages deployment

--- src/components/Header.js
+++ src/components/Header.js
@@ -134,7 +134,7 @@ const Header = () => {
         </ul>
         <a
           className="resume-button"
-          href="/resume.pdf"
+          href={`${process.env.PUBLIC_URL}/resume.pdf`}
           target="_blank"
           rel="noopener noreferrer"
         >
